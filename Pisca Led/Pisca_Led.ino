f0ead8f5876bf76e2ec593396fd8b05291027017
Luiz Fernando S. da Silveira <luizfernando@linzer3l.github.com>
2017-10-18 13:39:34 -0200

    Código
    Código do Pisca Led.

---
+++ Pisca Led/Pisca_Led.ino
@@ -0,0 +1,42 @@
+void setup() 
+{
+  pinMode(11, OUTPUT); //Saída de corrente elétrica.
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(11, HIGH); //Liga led.
+  delay(100);             //Intervalo de tempo.
+  digitalWrite(11, LOW);  //Desliga led.
+  delay(100);             //Intervalo de tempo.
+  
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+
+  digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);
+  delay(100);
+  
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  
+}
+
